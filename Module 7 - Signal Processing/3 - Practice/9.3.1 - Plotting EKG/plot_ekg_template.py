36b7ce5e14a9171ad3a5e840887b1eb6b566f192
MackLucki <luckinmg@dukes.jmu.edu>
2024-10-29 10:59:40 -0400

    .

--- Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.1 - Plotting EKG/plot_ekg_template.py
@@ -1,4 +1,4 @@
-
+import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -8,11 +8,17 @@ path = '../../../data/ekg/mitdb_201.csv'
 # load data in matrix from CSV file; skip first two rows
 
 ### Your code here ###
-
+Data = np.loadtxt(path, skiprows = 2, delimiter = ',')
 # save each vector as own variable
-
-### Your code here ###
+Time = Data[:, 0]
+MLII = Data[:, 1]
+Voltage = Data[:, 2]
 
 # use matplot lib to generate a single
 
-### Your code here ###
+plt.plot(Time, Voltage)
+plt.title('Voltages vs. Time')
+plt.xlabel('Time (s)')
+plt.ylabel('Voltage (V)')
+plt.legend()
+plt.show()
--- Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
+++ Module 7 - Signal Processing/3 - Practice/9.3.2 - Processing EKG Data/ekg-signal-processing-template.py
@@ -18,33 +18,58 @@ signal_filepath = path_to_folder + database_name + ".csv"
 Step #1: load data in matrix from CSV file; skip first two rows. Call the data signal.
 """
 
-signal = 0
-## YOUR CODE HERE ##
+signal = np.loadtxt(signal_filepath, skiprows=2, delimiter=',')
+
+Time = signal[:, 0]
+MLII = signal[:, 1]
+Voltage = signal[:, 2]
+
+plt.title('Process Signal for ' + database_name)
+plt.plot(Time, Voltage)
+plt.xlabel('Time (s)')
+plt.ylabel('Voltage')
+plt.show()
 
 """
 Step 2: (OPTIONAL) pass data through LOW PASS FILTER (fs=250Hz, fc=15, N=6). These may not be correctly in radians
 """
 
-## YOUR CODE HERE ##
-
 """
 Step 3: Pass data through weighted differentiator
 """
 
-## YOUR CODE HERE ##
-
+Diff_time = np.diff(Time)
+Diff_volt = np.diff(Voltage)
+plt.plot(Diff_volt)
+plt.title('Differential signal for ' + database_name)
+plt.xlabel('Time (s)')
+plt.ylabel('Voltage (V)')
+plt.show()
 
 """
 Step 4: Square the results of the previous step
 """
- ## YOUR CODE HERE ##
+Square_time = np.square(Diff_time)
+Square_volt = np.square(Diff_volt)
+plt.plot(Square_volt)
+plt.title('Squared signal for ' + database_name)
+plt.xlabel('Time (s)')
+plt.ylabel('Voltage (V)')
+plt.show()
 
 """
 Step 5: Pass a moving filter over your data
 """
+ones = np.ones(5)
+MovAverage_time = np.convolve(Square_time,ones)
+MovAverage_volt = np.convolve(Square_volt,ones)
+plt.plot(MovAverage_volt)
+plt.title('Moving average signal for ' + database_name)
+plt.xlabel('Time (s)')
+plt.ylabel('Voltage (V)')
+plt.show()
 
 ## YOUR CODE HERE
 # make a plot of the results. Can change the plot() parameter below to show different intermediate signals
-plt.title('Process Signal for ' + database_name)
-plt.plot(signal)
-plt.show()
+
+
